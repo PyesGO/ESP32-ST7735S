f6df2c6e703929a49f20f4ba6d35b997a33bce3e
yjtfx <yjtfxgo@163.com>
2024-09-03 13:33:07 +0800

    ...

--- main/main.c
+++ main/main.c
@@ -12,29 +12,17 @@ app_main(void) {
         .BLK = 33
     };
     st7735s_size size = {
-        .width = 16,
-        .height = 16
+        .width = 132,
+        .height = 132
     };
-    st7735s_buffer buffer = st7735s_buffer_init(4096);
-    
-    unsigned char count;
-    unsigned short int color;
+    st7735s_buffer buffer = st7735s_buffer_init(2048);
 
     st7735s_init(&pins, &size);
-    st7735s_blkctl(&pins, 1);
-    
-    color = 0;
-    for (count = 0; ((count < 50) && (color < 0xFFFF)); ++count) {
-        st7735s_fill_screen(&pins, &size, color, &buffer);
-        timesleep_ms(500);
-        color += 0x20;
-    }
-
-    // st7735s_fill_screen(&pins, &size, 0x00, );
-    // st7735s_draw_point(&pins, 66, 66, 0xFFFF);
-    // timesleep_ms(10000);
+    st7735s_fill_screen(&pins, &size, 0x00, &buffer);
     st7735s_buffer_free(&buffer);
+    st7735s_blkctl(&pins, 1);
+
+    timesleep_ms(5000);
     st7735s_blkctl(&pins, 0);
-    timesleep_ms(120);
     st7735s_powerctl(&pins, 0);
 }
