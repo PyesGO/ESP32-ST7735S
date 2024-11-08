892eeec7b9e2f2dd8fd6d57d4e30e994b66536b1
yjtfx <yjtfxgo@163.com>
2024-11-09 01:35:22 +0800

    No more changes.

--- main/st7735s_fontlib.c
+++ main/st7735s_fontlib.c
@@ -1,25 +1,26 @@
 #include "st7735s_fontlib.h"
 #include "st7735s_driver.h"
+#include "softSPI_lib.h"
 
 void
-st7735s_font_write_text(st7735s_pins *pins, unsigned char char_, unsigned short int color) {
+st7735s_font_write_text(st7735s_Screen *screen, unsigned char char_, unsigned short int color) {
     unsigned char byte_pixel, count, inter_count;
     const unsigned char *font_map_addr;
 
     font_map_addr = ST7735S_FONTS_MAP[char_];
-    st7735s_enable_transmit(pins);
-    st7735s_set_window_addr(pins, 16, 16, 31, 31);
-    st7735s_set_SRAM_writable(pins);
+    softSPI_enable_transmit(screen->pins);
+    st7735s_set_window_addr(screen, 16, 16, 32, 32);
+    st7735s_set_SRAM_writable(screen->pins);
     for (count = 0; count < 32; ++count) {
         byte_pixel = *(font_map_addr++);
         for (inter_count = 0; inter_count < 8; ++inter_count) {
             if (byte_pixel & 0x80) {
-                st7735s_write_color(pins, color);
+                softSPI_write_color(screen->pins, color);
             } else {
-                st7735s_write_color(pins, 0x0000);
+                softSPI_write_color(screen->pins, 0x0000);
             }
             byte_pixel <<= 1;
         }
     }
-    st7735s_disable_transmit(pins);
+    softSPI_disable_transmit(screen->pins);
 }
